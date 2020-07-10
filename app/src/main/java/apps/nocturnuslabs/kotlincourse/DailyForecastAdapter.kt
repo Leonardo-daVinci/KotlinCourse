15d00051397b0fa55b93dafebf77d5cc315c9918
Akshit Keoliya <akshitkeoliya4@gmail.com>
2020-07-11 05:01:58 +0530

    Completed Week 3 - RecyclerView Implementation

---
+++ app/src/main/java/apps/nocturnuslabs/kotlincourse/DailyForecastAdapter.kt
@@ -0,0 +1,64 @@
+package apps.nocturnuslabs.kotlincourse
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class DailyForecastViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    private val tempText = view.findViewById<TextView>(R.id.temptext)
+    private val descriptionText = view.findViewById<TextView>(R.id.descriptiontext)
+
+    fun bind(dailyForecast: DailyForecast) {
+        tempText.text = String.format("%.2f",dailyForecast.temp)
+        descriptionText.text = dailyForecast.description
+    }
+}
+
+class DailyForecastAdapter(
+    //this is used to implement clicking on individual list items
+    private val clickHandler : (DailyForecast)-> Unit
+) : ListAdapter<DailyForecast, DailyForecastViewHolder>(DIFF_CONFIG) {
+
+    //anything written here would exist outside the Adapter class
+    //it is similar to static in Java
+    companion object{
+
+        val DIFF_CONFIG = object : DiffUtil.ItemCallback<DailyForecast>(){
+            //object refers to instance of an anonymous class
+            //itemcallback will work on DailyForecast items
+
+            override fun areItemsTheSame(oldItem: DailyForecast, newItem: DailyForecast): Boolean {
+               // === operator is used to check if they are same object references
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: DailyForecast,
+                newItem: DailyForecast
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyForecastViewHolder {
+        //this creates a new view holder within which we create a new view which would represent each item in our list
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_daily_forecast, parent, false)
+        return DailyForecastViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: DailyForecastViewHolder, position: Int) {
+        //get forecast items and pass that to view holder so that view items can be updated.
+        holder.bind(getItem(position))
+        holder.itemView.setOnClickListener {
+            clickHandler(getItem(position))
+        }
+    }
+}
--- app/src/main/java/apps/nocturnuslabs/kotlincourse/MainActivity.kt
+++ app/src/main/java/apps/nocturnuslabs/kotlincourse/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +22,30 @@ class MainActivity : AppCompatActivity() {
 
         enterButton.setOnClickListener {
             val zipcode : String = zipcodeEditText.text.toString()
+            forecastRepository.loadForecast(zipcode)
         }
+
+        // region RecyclerView Implementation
+        val forecastList : RecyclerView = findViewById(R.id.main_forecast_list)
+
+        //we'll need a layout manager to know how the items should be laid out on the screen
+        forecastList.layoutManager = LinearLayoutManager(this)
+
+        //Steps for Adapter  - viewholder, adapter and item callback
+        val dailyForecastAdapter = DailyForecastAdapter(){
+            val message = getString(R.string.forecast_list_format, it.temp, it.description)
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
+        forecastList.adapter = dailyForecastAdapter
+
+        //endregion RecyclerView
+
 
         //adding observer for the repository which updates anytime Livedata is updated in the repo
         val weeklyForecastObserver = Observer<List<DailyForecast>>{forecastItems ->
             //update our list adapter
+            Toast.makeText(this, "Loaded Items",Toast.LENGTH_SHORT).show()
+            dailyForecastAdapter.submitList(forecastItems)
         }
 
         forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver) //observe function requires a lifecycle owner and an observer
